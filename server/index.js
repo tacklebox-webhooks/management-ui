b1dfc8f56518e208ad0863625e4f8d5e321c667b
Kevin Counihan <kevin.counihan17@gmail.com>
2021-05-12 16:39:43 -0700

    pages now are served correctly when navigating directly to non-root paths

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const path = require("path");
 
 app.use(express.static("build"));
+app.get("*", (req, res) => res.sendFile(path.resolve("build", "index.html")));
 app.listen(port, () => {
   console.log(`View your admin dashboard at http://localhost:${port}`);
 });
