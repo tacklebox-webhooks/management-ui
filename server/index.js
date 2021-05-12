48b4571f4a4cfa05a01a47fec312c2c23358d1b3
ArmandoMota <armandomotagt@gmail.com>
2021-05-12 15:54:54 -0700

    Added express server to run ui locally and put client code into client folder
    Co-authored-by: Juan Palma <juanpedropalma@hotmail.com>
    Co-authored-by: Kevin Counihan <kevin.counihan17@gmail.com>
    Co-authored-by: Kayl Thomas <kaylthomas607@gmail.com>

---
+++ server/index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.static("build"));
+app.listen(port, () => {
+  console.log(`View your admin dashboard at http://localhost:${port}`);
+});
