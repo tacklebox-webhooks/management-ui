7aeefbaa826f9ea5371eff29166240d6a531e910
Kevin Counihan <kevin.counihan17@gmail.com>
2021-05-12 18:34:31 -0700

    Fixed reducer returning undefined in some cases

--- client/src/reducers/currentService.js
+++ client/src/reducers/currentService.js
@@ -5,6 +5,8 @@ const currentService = (state = { name: "" }, action) => {
     case types.FETCH_SERVICES_SUCCESS:
       if (action.services.length > 0 && state.name === "") {
         return action.services[0];
+      } else {
+        return state;
       }
     case types.SET_CURRENT_SERVICE_SUCCESS:
       return action.currentService;
